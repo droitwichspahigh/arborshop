c80500724f5854721bb400f5cfb40a9c03488062
Chris Rees <crees@FreeBSD.org>
2020-04-22 20:07:00 +0100

    Form correct HTML.

--- bin/db_connect.php
+++ bin/db_connect.php
@@ -5,5 +5,5 @@ require ("config.php");
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, "purchases");
 
 if (!$conn) {
-    die ("Database connection failure<br>");
+    die ("Database connection failure<br />");
 }
