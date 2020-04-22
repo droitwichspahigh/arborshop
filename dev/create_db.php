ea52bedf9e29b6344224d66b133a15d6fd6821ab
Chris Rees <crees@FreeBSD.org>
2020-04-22 19:58:25 +0100

    Move SQL execution to function

--- dev/create_db.php
+++ dev/create_db.php
@@ -4,29 +4,21 @@
 require "../bin/auth.php";
 require "../bin/db_connect.php";
 
+if ($lets_start_right_from_the_beginning != TRUE)
+    die ("You don't really want to do this.  Check config.php");
+    
 echo "Connected successfully<br /><br />";
 
-if ($lets_start_right_from_the_beginning != TRUE)
-    die ("You don't want to do this.  Check config.php");
-
-$sql = "DROP DATABASE $dbname";
-
-if(mysqli_query($conn, $sql)) {
-	echo "$dbname database deleted successfully";
-} else {
-	echo "Error deleting $dbname database: " . mysqli_error($conn);
+function dosql($sqlcmd) {
+    if (mysqli_query($conn, $sqlcmd)) {
+        echo "$sqlcmd performed successfully<br /><br />";
+    } else {
+        die ("Error in $sqlcmd : " . mysqli_error($conn));
+    }
 }
 
-echo "<br /><br />";
-
-echo $sql;
-if(mysqli_query($conn, $sql)) {
-	echo "$dbname database created successfully";
-} else {
-	echo "Error creating $dbname database: " . mysqli_error($conn);
-}
-
-mysqli_close($conn);
+dosql("DROP DATABASE $dbname;");
+dosql("CREATE DATABASE $dbname;");
 
 
 ?>
