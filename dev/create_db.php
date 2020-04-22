cdf70b34017b9032f552f8350cb976bccafb57b8
Chris Rees <crees@FreeBSD.org>
2020-04-22 20:45:06 +0100

    Add basic db structure
    While here, don't error if database doesn't already exist.

--- dev/create_db.php
+++ dev/create_db.php
@@ -3,18 +3,19 @@
 
 require ("../bin/auth.php");
 
+if ($lets_start_right_from_the_beginning != TRUE)
+    die ("You don't really want to do this.  Check config.php");
+    
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
 
 if (!$conn) {
     die ("Database connection failure<br>");
 }
 
-if ($lets_start_right_from_the_beginning != TRUE)
-    die ("You don't really want to do this.  Check config.php");
-    
 echo "Connected successfully<br /><br />";
 
 function dosql($sqlcmd) {
+    global $conn;
     if (mysqli_query($conn, $sqlcmd)) {
         echo "$sqlcmd performed successfully<br /><br />";
     } else {
@@ -22,10 +23,21 @@ function dosql($sqlcmd) {
     }
 }
 
-dosql("DROP DATABASE $dbname;");
+mysqli_query($conn, "DROP DATABASE $dbname;"); /* Don't mind if this fails */
 dosql("CREATE DATABASE $dbname;");
 dosql("USE $dbname;");
-dosql("CREATE TABLE purchases ");
+$sql = <<< EOT
+    CREATE TABLE purchases (
+        purchase_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        upn VARCHAR(13) NOT NULL,
+        datetime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        price SMALLINT UNSIGNED NOT NULL,
+        item_id INT UNSIGNED NOT NULL,
+        CONSTRAINT purchases_pk PRIMARY KEY (purchase_id)
+    );
+EOT;
+dosql($sql);
+dosql("CREATE TABLE spent (upn VARCHAR(13) NOT NULL, spent SMALLINT UNSIGNED NOT NULL DEFAULT 0);")
 
 ?>
 </html>
