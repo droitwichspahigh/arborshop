2fc92ed58fb3c51d4e68def8c8074f08354d8116
Chris Rees <crees@FreeBSD.org>
2020-04-22 15:43:07 +0100

    Add staff area.

---
+++ staff/index.php
@@ -0,0 +1,12 @@
+<?php
+require ('../bin/auth.php');
+?>
+<html lang="en">
+
+<?php require ('../bin/head.php');?>
+
+<body>
+	<h1>Welcome to the staff area of <?= $site_name ?>!</h1>
+</body>
+
+</html>
