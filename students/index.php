74b5020077601744287e5e088efc0b4a6a4fa3ea
Chris Rees <crees@FreeBSD.org>
2020-04-23 15:16:23 +0100

    Flesh out student homepage

--- students/index.php
+++ students/index.php
@@ -2,11 +2,28 @@
 require ('../bin/auth.php');
 ?>
 <html lang="en">
-
-<?php require ('../bin/head.php');?>
+<head>
+	<?php require ('../bin/head.php');?>
+	<link href="../css/students.css" rel="stylesheet">
+</head>
 
 <body>
-	<h1>Welcome to the student area of <?= $site_name ?>!</h1>
+	<div class="container">
+		<div class="row justify-content-md-center">
+			<div class="col-lg-9">
+				<div class="card border-primary">
+					<div class="card-body">
+						<img class="mb-4" src="../img/logo_v2.jpg" alt="" height="72">
+						<h1 class="h3 font-weight-normal">Welcome to <?= $site_name; ?></h1>
+						<div id="user-details">
+							<!-- Here we're going to fill in the user's name, net points and balance once fetched from Arbor -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
 </body>
 
 </html>
