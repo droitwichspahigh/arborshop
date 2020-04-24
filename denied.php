70f52479c00fdd3b0f37c51278b765fed0c69eb6
Chris Rees <crees@FreeBSD.org>
2020-04-24 15:16:45 +0100

    Be explicit about why denial is happening.

--- denied.php
+++ denied.php
@@ -15,7 +15,14 @@ require('bin/config.php');
 		<div class="card-body">
 			<form action="index.php" method="post">
 				<a href="./"><img class="mb-4" src="img/times-circle.svg" alt="" width="72" height="72"></a>
-				<p class="text-footer text-muted">Access Denied: You are not authorised to use this resource. Please contact <a href="mailto:<?= $support_email ?>?subject=<?= $brand_name; ?>: Access Request"><?= $powered_by ?></a> if this is an error.</p>
+				<p class="text-footer text-muted">				
+					<?php if (!$maintenance) { ?>
+						Access Denied: You are not authorised to use this resource.
+						Please contact <a href="mailto:<?= $support_email ?>?subject=<?= $site_name; ?>: Access Request">
+							<?= $powered_by ?></a> if this is an error.
+					<?php } else { ?>
+				        <?= $site_name; ?> is currently under maintenance.  Please try again later.<?php } ?>
+				</p>
 				<p class="text-footer text-muted">You're trying to access this site as <?= $_SERVER['PHP_AUTH_USER'];?></p>
 			</form>
 		</div>
