abda50062689617122314f63a50f6342a609429e
Chris Rees <crees@FreeBSD.org>
2020-04-24 15:50:39 +0100

    Add Arbor connector

---
+++ bin/arbor_connection.php
@@ -0,0 +1,26 @@
+<?php
+
+require "config.php";
+
+/**
+ * Connection details to Arbor
+ */
+$arborconfig = [
+    'api' => [
+        'baseUrl' => "$arborsite/rest-v2/",
+        'auth' => [
+            'user' => $arboruser,
+            'password' => $arborpassword
+        ]
+    ]
+];
+
+$api = new \Arbor\Api\Gateway\RestGateway(
+    $arborconfig['api']['baseUrl'],
+    $arborconfig['api']['auth']['user'],
+    $arborconfig['api']['auth']['password']
+    );
+
+\Arbor\Model\ModelBase::setDefaultGateway($api);
+
+date_default_timezone_set('Europe/London');
