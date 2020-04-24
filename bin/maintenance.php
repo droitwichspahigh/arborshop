cf5481b6873de3703c7153e5c0acb888f81fb7bb
Chris Rees <crees@FreeBSD.org>
2020-04-24 14:19:05 +0100

    Separate maintenance switches into separate file

---
+++ bin/maintenance.php
@@ -0,0 +1,15 @@
+<?php
+/* Here you can change parts to temporarily allow certain actions */ 
+
+/* Not a good idea to disable this while maintenance mode active,
+ * otherwise people will be allowed to login and you might forget... */
+$maintenance = TRUE;
+
+/* Might as well leave this enabled */
+$debug = TRUE;
+
+/* Warning, this enables the dangerous database operations */
+$installer_mode = TRUE;
+
+/* This is only for if you want to delete everything */
+$lets_start_right_from_the_beginning = TRUE;
