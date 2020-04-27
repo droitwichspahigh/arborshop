e6f81e46701ef0f9099a13a623ef2119ad98b351
Chris Rees <crees@FreeBSD.org>
2020-04-27 16:22:27 +0100

    Add primary key to spent

--- dev/newyear.php
+++ dev/newyear.php
@@ -1,7 +1,6 @@
 <?php
 
 dosql("DROP TABLE purchases;", FALSE);
-
 $sql = <<< EOT
     CREATE TABLE purchases (
         purchase_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -13,8 +12,16 @@ $sql = <<< EOT
         CONSTRAINT purchases_pk PRIMARY KEY (purchase_id)
     );
 EOT;
+dosql($sql);
 
 dosql("DROP TABLE spent", FALSE);
 
+$sql = <<< EOT
+    CREATE TABLE spent (
+        spent_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        upn VARCHAR(13) NOT NULL,
+        spent SMALLINT UNSIGNED NOT NULL DEFAULT 0,
+        CONSTRAINT spent_pk PRIMARY KEY (spent_id)
+    );
+EOT;
 dosql($sql);
-dosql("CREATE TABLE spent (upn VARCHAR(13) NOT NULL, spent SMALLINT UNSIGNED NOT NULL DEFAULT 0);");
