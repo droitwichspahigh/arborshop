7348b984d8d4e3a0acd5dc4c56226c09d3bd2cc5
Chris Rees <crees@FreeBSD.org>
2020-04-27 16:23:36 +0100

    Safe defaults

--- bin/maintenance.php
+++ bin/maintenance.php
@@ -12,24 +12,24 @@ $maintenance = TRUE;
 /**
  * Set to TRUE to echo warnings and notices from SQL.
  */
-$debug = TRUE;
+// $debug = TRUE;
 
 /**
  * Everything inside the /dev hierarchy depends on this being TRUE
  */
-$installer_mode = TRUE;
+// $installer_mode = TRUE;
 
 /**
  * Enable this to drop all the student records from the previous year.
  * 
  * A September job!
  */
-$new_year = TRUE;
+// $new_year = TRUE;
 
 /**
  * This means the ENTIRE database is going to be dropped and remade
  */
-$lets_start_right_from_the_beginning = FALSE;
+// $lets_start_right_from_the_beginning = TRUE;
 
 /** Set up some PHP error reporting too */
 
