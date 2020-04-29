f7ffc157321e76a5cfa7e38bb13bd841dd55f00b
Chris Rees <crees@FreeBSD.org>
2020-04-29 17:25:26 +0100

    Just use bootstrap directly for signin

--- denied.php
+++ denied.php
@@ -3,28 +3,24 @@ require('bin/config.php');
 ?>
 <html lang="en">
 
-<?php include('bin/head.php');?>
-
 <head>
-	<link href="css/signin.css" rel="stylesheet">
+	<?php include('bin/head.php');?>
 </head>
 
 <body class="text-center">
 
-	<div class="card border-danger">
+	<div class="card text-center border-danger mx-auto my-5" style="width: 18rem">
 		<div class="card-body">
-			<form action="index.php" method="post">
-				<a href="./"><img class="mb-4" src="img/times-circle.svg" alt="" width="72" height="72"></a>
-				<p class="text-footer text-muted">				
-					<?php if (!$maintenance) { ?>
-						Access Denied: You are not authorised to use this resource.
-						Please contact <a href="mailto:<?= $support_email ?>?subject=<?= $site_name; ?>: Access Request">
-							<?= $powered_by ?></a> if this is an error.
-					<?php } else { ?>
-				        <?= $site_name; ?> is currently under maintenance.  Please try again later.<?php } ?>
-				</p>
-				<p class="text-footer text-muted">You're trying to access this site as <?= $_SERVER['PHP_AUTH_USER'];?></p>
-			</form>
+			<a href="./"><img class="mb-4" src="img/times-circle.svg" alt="" width="72" height="72"></a>
+			<p class="text-footer text-muted">				
+				<?php if (!$maintenance) { ?>
+					Access Denied: You are not authorised to use this resource.
+					Please contact <a href="mailto:<?= $support_email ?>?subject=<?= $site_name; ?>: Access Request">
+						<?= $powered_by ?></a> if this is an error.
+				<?php } else { ?>
+			        <?= $site_name; ?> is currently under maintenance.  Please try again later.<?php } ?>
+			</p>
+			<p class="text-footer text-muted">You're trying to access this site as <?= $_SERVER['PHP_AUTH_USER'];?></p>
 		</div>
 	</div>
 	
--- index.php
+++ index.php
@@ -16,22 +16,19 @@ if(isset($_SERVER['PHP_AUTH_USER'])) {
 ?>
 <html lang="en">
 
-<?php include('bin/head.php');?>
-
 <head>
-	<link href="css/signin.css" rel="stylesheet">
+	<?php include('bin/head.php');?>
 </head>
 
-<body class="text-center">
-
-	<div class="card border-primary">
+<body>
+	<div class="card text-center border-primary mx-auto my-5" style="width: 18rem">
 		<div class="card-body">
 			<form action="index.php" method="post">
-				<img class="mb-4" src="img/logo_v2.jpg" alt="" height="72">
-				<h1 class="h3 mb-3 font-weight-normal"><small><?= $site_name; ?></small></h1>
+				<img class="mb-4 w-100" src="img/logo_v2.jpg" alt="Droitwich Spa High School">
+				<h3 class="mb-3"><?= $site_name; ?></h1>
 				<button type="submit" name="authenticate" class="btn btn-lg btn-primary btn-block mb-3">Sign in</button>
-				<small class="text-muted">Please enter your school username and password when prompted.</small>
-				<hr>
+				<p class="small text-muted">Please enter your school username and password when prompted.</p>
+				<hr />
 				<p class="text-footer text-muted">Powered by <a href="mailto:<?= $support_email; ?>?subject=<?= $brand_name; ?>"><?= $powered_by ?></a><br><small><?php echo $version; ?></small></p> 
 			</form>
 		</div>
