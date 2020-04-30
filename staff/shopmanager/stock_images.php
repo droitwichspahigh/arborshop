ba5fc5ba7f3d005a6626db278c73596a34d0119d
Chris Rees <crees@FreeBSD.org>
2020-04-30 14:18:33 +0100

    Easiest way to fix permissions is to just recreate the directory

--- staff/shopmanager/stock_images.php
+++ staff/shopmanager/stock_images.php
@@ -1,4 +1,8 @@
 <?php
 
 require "../../bin/auth.php";
+
+if (!file_exists($_SERVER['DOCUMENT_ROOT'] . "/shop/img/product"))
+    mkdir($_SERVER['DOCUMENT_ROOT'] . "/shop/img/product");
+
 require "../../bin/tinyfilemanager.php";
