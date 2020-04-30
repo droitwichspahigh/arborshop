eacf35173d23822360ac283901067d12a0deb7fa
Chris Rees <crees@FreeBSD.org>
2020-04-30 15:50:48 +0100

    Make the shop manager page look less hideous

--- staff/shopmanager/index.php
+++ staff/shopmanager/index.php
@@ -6,13 +6,29 @@ require ('../../bin/auth.php');
 <?php require ('../../bin/head.php');?>
 
 <body>
-	<h1>Welcome to the Stock Management area of <?= $site_name ?>!</h1>
-	
-	<p>Please upload images to the folder <a href="stock_images.php">here</a>.</p>
-	
-	<p>Please then enter the details of each product <a href="stockeditor/CkEditor.php">here</a>.</p>
-	
-	<p>It is often helpful to have both of these open, perhaps in separate windows (Shift+click does this), so that you can keep track of the names of the images</p>
+	<div class="card text-center border-secondary mx-auto my-5" style="width: 22rem;">
+    	<div class="card-body">
+	  		<img class="mb-4 w-100" src="../../img/logo_v2.jpg" alt="Droitwich Spa High School" />
+	  		
+        	<h4 class="card-title"><?= $site_name ?> Stock Management area</h4>
+        	
+        	<p class="card-text">First upload images to the folder.
+        		The default images are provided <a href="<?= $site_url; ?>/img/default_stock_images.zip">here</a> in case you delete any by mistake.</p>
+        	
+        	<p class="card-text">Please then enter the details of each product.</p>
+        	
+        	<p><a href="stock_images.php" class="btn btn-warning">Image manager</a>
+        						 <a href="stockeditor/CkEditor.php" class="btn btn-warning">Stock details editor</a></p>
+        	
+        	<p class="card-text text-muted">It is often helpful to have both of these open, perhaps in
+        		separate windows (Shift+click does this), so that you can keep track of the names of the images</p>
+
+        	<div class="alert alert-danger" role="alert">
+  				<strong>Take care!</strong> Do not use double quotes or other special characters in names, descriptions or other fields.
+  					Rather than deleting items, it's nearly always better to disable them and put a note into the description box, as it may mess up previous purchases. 
+			</div>
+    	</div>
+    </div>
 </body>
 
 </html>
