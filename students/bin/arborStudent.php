18cab2afa0f0dfc3f6f4cc2c013fa25095b66181
Chris Rees <crees@FreeBSD.org>
2020-05-04 14:45:43 +0100

    Pull total points out of Arbor and greet by name.

---
+++ students/bin/arborStudent.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * From arbor_connection.php:
+ * @var \Arbor\Api\Gateway\RestGateway  $api
+ * @var array                           $arborconfig
+ * 
+ * From config.php:
+ * @var string                          $site_emaildomain
+ * 
+ * From masquerade.php:
+ * @var string                          $student
+ * */
+
+require 'masquerade.php';
+
+/** @var int $points 
+ * Subtract behaviour points, and add good conduct points */
+$points = 0;
+
+$emailQuery = new \Arbor\Query\Query(\Arbor\Resource\ResourceType::EMAIL_ADDRESS);
+/* This is where we'll query network login */
+$emailQuery->addPropertyFilter(\Arbor\Model\EmailAddress::EMAIL_ADDRESS, \Arbor\Query\Query::OPERATOR_EQUALS, "$student@$site_emaildomain");
+
+$emailAddress = \Arbor\Model\EmailAddress::query($emailQuery);
+
+/* Awesome, so we can get the Arbor ID of the pupil, and all is unlocked */
+
+$arborStudentId = $emailAddress[0]->getEmailAddressOwner()->getProperty('id');
+$arborStudentHref = "rest-v2/students/$arborStudentId";
+/** @var \Arbor\Model\Student $arborStudent */
+$arborStudent =  $api->retrieve(\Arbor\Resource\ResourceType::STUDENT, $arborStudentId);
+
+/* So... we get a list of incidents for x points, for all severity values.
+ * 
+ * We sum the values to get points.
+ */
+for ($pointValue = -4; $pointValue <= 4; $pointValue++) {
+    /* Now get behavioural incidents */
+    $behaviourQuery = new \Arbor\Query\Query(\Arbor\Resource\ResourceType::BEHAVIOURAL_INCIDENT_STUDENT_INVOLVEMENT);
+    
+    /* So... I have to hand-hack to look up by ArborId.  Really?  This *is* going to break at some point :( */
+    $behaviourQuery->addPropertyFilter(
+        "student",
+        \Arbor\Query\Query::OPERATOR_EQUALS,
+        $arborStudentHref);
+    
+    $behaviourQuery->addPropertyFilter(
+        \Arbor\Model\BehaviouralIncidentStudentInvolvement::SEVERITY,
+        \Arbor\Query\Query::OPERATOR_EQUALS,
+        $pointValue);
+    
+    $behaviourQuery->addPropertyFilter(
+        \Arbor\Model\BehaviouralIncidentStudentInvolvement::BEHAVIOURAL_INCIDENT
+            . '.'
+            . \Arbor\Model\BehaviouralIncident::INCIDENT_DATETIME,
+        \Arbor\Query\Query::OPERATOR_AFTER,
+        gmdate("Y-m-d", strtotime("first day of September last year")));
+    
+    $points += $pointValue * sizeof($api->query($behaviourQuery));
+}
+
+/* Can't believe I have to do it the above way- this way is way
+ * too slow because of the huge pages I have to download for each
+ * Incident. 
+ */
+$multiline_comment = <<< 'EOF'
+
+echo "<pre>";
+
+ foreach ($api->query($behaviourQuery) as $model) {
+ $hydrator = new \Arbor\Model\Hydrator();
+ print_r($hydrator->extractArray($model));
+ }
+
+
+foreach ($api->query($behaviourQuery) as $b) {
+    //$behaviouralIncidentStudentInvolvement = $api->retrieve(\Arbor\Resource\ResourceType::BEHAVIOURAL_INCIDENT_STUDENT_INVOLVEMENT, $b->getProperty('id'));
+    //$points += $behaviouralIncidentStudentInvolvement->getBehaviouralIncident()->getSeverity();
+
+//$points += $behaviouralIncidentStudentInvolvement->getSeverity();
+}
+
+echo "Your points balance; $points";
+
+//$modelCollection = $api->query($behaviourQuery);
+
+/*
+foreach ($modelCollection as $model) {
+    $hydrator = new \Arbor\Model\Hydrator();
+    print_r($hydrator->extractArray($model));
+}
+*/
+
+die("Done");
+
+EOF;
+
+
+
--- students/index.php
+++ students/index.php
@@ -1,7 +1,7 @@
 <?php
 require '../bin/auth.php';
 require '../bin/arbor_connection.php';
-require 'bin/masquerade.php';
+require 'bin/arborStudent.php';
 
 ?>
 <html lang="en">
@@ -17,9 +17,9 @@ require 'bin/masquerade.php';
 				<div class="card border-primary">
 					<div class="card-body">
 						<img class="mb-4" src="../img/logo_v2.jpg" alt="" height="72">
-						<h1 class="h3 font-weight-normal">Welcome to <?= $site_name; ?>, <?= $student; ?></h1>
+						<h1 class="h3 font-weight-normal">Welcome to <?= $site_name; ?>, <?= $arborStudent->getPerson()->getPreferredFirstName(); ?>.</h1>
 						<div id="user-details">
-							<!-- Here we're going to fill in the user's name, net points and balance once fetched from Arbor -->
+							<?php echo $arborStudent->getPerson()->getPreferredFirstName(); ?>
 						</div>
 					</div>
 				</div>
