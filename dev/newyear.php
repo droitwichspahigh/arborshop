aadbc3553f63d71aa8eb4c7418794e4a5e7fc93d
Chris Rees <crees@FreeBSD.org>
2020-05-04 10:57:00 +0100

    We're going to use Arbor's internal ID instead of UPN.
    This is actually great for GDPR purposes, as we'll store literally no
    identifiable data, and there's nothing anyone can do if they steal
    the database, as it's linked to a meaningless ID.
    
    I'm hoping Arbor doesn't ever change student IDs... as they're
    primary keys I think it unlikely.

--- dev/newyear.php
+++ dev/newyear.php
@@ -4,7 +4,7 @@ dosql("DROP TABLE purchases;", FALSE);
 $sql = <<< EOT
     CREATE TABLE purchases (
         purchase_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-        upn VARCHAR(13) NOT NULL,
+        arbor_id INT NOT NULL,
         datetime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
         price SMALLINT UNSIGNED NOT NULL,
         item_id INT UNSIGNED NOT NULL,
@@ -19,7 +19,7 @@ dosql("DROP TABLE spent", FALSE);
 $sql = <<< EOT
     CREATE TABLE spent (
         spent_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-        upn VARCHAR(13) NOT NULL,
+        arbor_id INT NOT NULL,
         spent SMALLINT UNSIGNED NOT NULL DEFAULT 0,
         CONSTRAINT spent_pk PRIMARY KEY (spent_id)
     );
