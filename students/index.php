ef9a4d7d9659acdd32a0252024aba31fc656d0a1
Chris Rees <crees@FreeBSD.org>
2020-05-05 13:13:36 +0100

    No need for separate stylesheets, and style modification
    Also, we don't really want the shop appearing as a card.

--- students/index.php
+++ students/index.php
@@ -6,24 +6,16 @@ require 'bin/arborStudent.php';
 <html lang="en">
 <head>
 	<?php require ('../bin/head.php');?>
-	<link href="../css/students.css" rel="stylesheet">
 </head>
 
 <body>
+	<?php include "../bin/breadcrumbs.php"; ?>
 	<div class="container">
-		<div class="row justify-content-md-center">
-			<div class="col-lg-9">
-				<div class="card border-primary">
-					<div class="card-body">
-						<img class="mb-4" src="../img/logo_v2.jpg" alt="" height="72">
-						<h1 class="h3 font-weight-normal">Welcome to <?= $site_name; ?>, <?= $arborStudent->getPerson()->getPreferredFirstName(); ?>.</h1>
-						<div id="user-details">
-							You have <?= $points; ?> points to spend.  Please have a look at the products available for you below, and click on them to purchase.
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+    	<div class="text-center"><img class="mb-4 img-responsive" src="../img/logo_v2.jpg" alt="" height="72" /></div>
+    	<h1 class="h3 font-weight-normal">Welcome to <?= $site_name; ?>, <?= $arborStudent->getPerson()->getPreferredFirstName(); ?>.</h1>
+    	<div id="user-details">
+    		You have <?= $points; ?> points to spend.  Please have a look at the products available for you below, and click on them to purchase.
+    	</div>
 	</div>
 	
 </body>
