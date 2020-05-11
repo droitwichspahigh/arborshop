cbd71c9e42abbcfec84de6813c9c11821733b26a
Chris Rees <crees@FreeBSD.org>
2020-05-11 11:29:46 +0100

    I think we can now trust the behaviour points cache
    Give it five minutes to expire rather than 30 seconds.

--- bin/Student.php
+++ bin/Student.php
@@ -81,7 +81,7 @@ class Student {
         
         if ($pointsCache_query->num_rows > 0) {
             $row = $pointsCache_query->fetch_row();
-            if ($row[1] > (time() - 30)) {
+            if ($row[1] > (time() - 300)) {
                 $this->behaviourNetPoints = $row[0];
                 return $this->getBehaviourNetPoints();
             } else {
