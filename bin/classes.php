559491b3372b387266f21722f675817b0540e371
crees <crees@freebsd.org>
2020-05-15 15:17:11 +0100

    Don't have debug stuff in the live site.

--- bin/classes.php
+++ bin/classes.php
@@ -1,8 +1,6 @@
 <?php
 namespace ArborShop;
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
+
 /* Have we been called before? */
 if (!class_exists("ArborShop\Config")) {
     require 'Config.php';
